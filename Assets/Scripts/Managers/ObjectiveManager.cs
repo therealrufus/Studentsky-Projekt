292d99f79a0a7f345ca95292c28418c620a23ac8
Samuel Mráz <samko.mraz@seznam.cz>
2022-04-16 19:59:36 +0200

    win/lose

--- Assets/Scripts/Managers/ObjectiveManager.cs
+++ Assets/Scripts/Managers/ObjectiveManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class ObjectiveManager : MonoBehaviour
@@ -8,18 +9,24 @@ public class ObjectiveManager : MonoBehaviour
     public static ObjectiveManager instance;
     public PlayerMovement player;
     public Objective[] objectives;
-    [HideInInspector] public UnityEvent onNewObjective;
     public float minDistance = 10;
+    [Header("score")]
+    //https://www.desmos.com/calculator/fnvmmxu2vi
+    public float curveSteepness = 150;
+    public float curveStartTime = 30;
+    float timer;
+    int score;
+    public Text text;
+
+    [HideInInspector] public UnityEvent onNewObjective;
     [HideInInspector]
     public Objective currentObjective
     {
         get { return _currentObjective; }
-        set { _currentObjective = value; onNewObjective.Invoke();}
+        set { _currentObjective = value; onNewObjective.Invoke(); }
     }
 
     Objective _currentObjective;
-
-
 
     // Start is called before the first frame update
     void Awake()
@@ -31,8 +38,21 @@ public class ObjectiveManager : MonoBehaviour
         AssignNewObjective();
     }
 
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            GameOver();
+        }
+        text.text = Mathf.Round(timer).ToString();
+    }
+
     void AssignNewObjective()
     {
+        score++;
+        timer = GetTime(score);
+        
         Objective newObjective = null;
 
         for (int i = 0; i < 500; i++)
@@ -42,11 +62,24 @@ public class ObjectiveManager : MonoBehaviour
                 break;
         }
         currentObjective = newObjective;
-
     }
 
     public void PassedThroughObjective(Objective objective)
     {
-        if (objective == currentObjective) AssignNewObjective();
+        if (objective == currentObjective) 
+        {
+            AssignNewObjective();
+        }
+    }
+
+    float GetTime(float x)
+    {
+        float value = curveSteepness / (x + (curveSteepness / curveStartTime));
+        return value;
+    }
+
+    void GameOver()
+    {
+
     }
 }
